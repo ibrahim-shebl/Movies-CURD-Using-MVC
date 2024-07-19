5025614541905b28abba7c22da64d4dcfb91cbf6
ibrahim-shebl <hema01025946540@gmail.com>
2024-07-18 19:12:53 -0700

    first commit

---
+++ Movies CURD/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Movies_CURD.Models
+{
+    public class Genre
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public byte Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
